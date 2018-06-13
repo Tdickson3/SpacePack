734ab4a1b56badd28e635ce24f70c703950480fc
Seaton Jiang <seaton@vtrois.com>
2018-06-13 08:24:24 +0800

    Update system_info.sh
    Add the Tencent cloud info

--- tools/system_info.sh
+++ tools/system_info.sh
@@ -30,25 +30,35 @@ operation_system() {
 
 public_ip() {
     local IP=$( wget -qO- -t1 -T2 api.ip.la )
-    [ -z "${IP}" ] && IP=$( wget -qO- -t1 -T2 ip.vtrois.com )
-    [ ! -z "${IP}" ] && echo ${IP} || echo -e "${RGB_DANGER}Unknown${RGB_END}"
+    [ ! -z "${PIP}" ] && echo ${PIP} || echo -e "${RGB_DANGER}Unknown${RGB_END}"
 }
 
-MEMTOTAL=$( cat /proc/meminfo | grep MemTotal | awk -F" " '{total=$2/1024}{printf("%d MB",total)}' )
-MEMFREE=$( cat /proc/meminfo | grep MemFree | awk -F" " '{free=$2/1024}{printf("%d MB",free)}' )
-SWAPTOTAL=$( cat /proc/meminfo  | grep SwapTotal | awk -F" " '{total=$2/1024}{printf("%d MB",total)}' )
-SWAPFREE=$( cat /proc/meminfo  | grep SwapFree | awk -F" " '{free=$2/1024}{printf("%d MB",free)}' )
-CPUMODEL=$( cat /proc/cpuinfo | grep 'model name' | awk 'END{print}' | awk -F": " '{print $2}' )
-CPUMHZ=$( cat /proc/cpuinfo | grep 'cpu MHz' | awk 'END{print}' | awk -F": " '{print($2,"MHz")}' )
+MEMTOTAL=$( cat /proc/meminfo | grep "MemTotal" | awk -F" " '{total=$2/1000}{printf("%d MB",total)}' )
+MEMFREE=$( cat /proc/meminfo | grep "MemFree" | awk -F" " '{free=$2/1000}{printf("%d MB",free)}' )
+SWAPTOTAL=$( cat /proc/meminfo  | grep "SwapTotal" | awk -F" " '{total=$2/1000}{printf("%d MB",total)}' )
+SWAPFREE=$( cat /proc/meminfo  | grep "SwapFree" | awk -F" " '{free=$2/1000}{printf("%d MB",free)}' )
+CPUMODEL=$( cat /proc/cpuinfo | grep "model name" | awk 'END{print}' | awk -F": " '{print $2}' )
+CPUMHZ=$( cat /proc/cpuinfo | grep "cpu MHz" | awk 'END{print}' | awk -F": " '{print($2,"MHz")}' )
 CPUCORES=$( cat /proc/cpuinfo | awk -F: '/model name/ {core++} END {print core}' )
-CPUCACHE=$( cat /proc/cpuinfo | grep 'cache size' | awk 'END{print}' | awk -F": " '{print $2}' )
+CPUCACHE=$( cat /proc/cpuinfo | grep "cache size" | awk 'END{print}' | awk -F": " '{print $2}' )
 SYSOS=$( operation_system )
 SYSRISC=$( uname -m )
 SYSLBIT=$( getconf LONG_BIT )
 KERNEVERSIONL=$( cat /proc/version | awk -F" " '{print $3}' )
-PUBLICIP=$( public_ip )
-INTERNALIP=$( ifconfig | grep 'inet' | grep -v '127.0' | xargs | awk -F '[ :]' '{print $2}' )
 NAMESERVER=$( cat /etc/resolv.conf | awk '/^nameserver/{print $2}' | awk 'BEGIN{FS="\n";RS="";ORS=""}{for(x=1;x<=NF;x++){print $x"\t"} print "\n"}' )
+TENCENTCLOUD=$( wget -qO- -t1 -T2 metadata.tencentyun.com )
+if [ ! -z "${TENCENTCLOUD}" ]; then
+PUBLICIP=$( wget -qO- -t1 -T2 metadata.tencentyun.com/latest/meta-data/public-ipv4 )
+LOCALIP=$( wget -qO- -t1 -T2 metadata.tencentyun.com/latest/meta-data/local-ipv4 )
+MACADDRESS=$( wget -qO- -t1 -T2 metadata.tencentyun.com/latest/meta-data/mac )
+INSTANCEID=$( wget -qO- -t1 -T2 metadata.tencentyun.com/latest/meta-data/instance-id )
+UUID=$( wget -qO- -t1 -T2 metadata.tencentyun.com/latest/meta-data/uuid )
+REGIONZONE=$( wget -qO- -t1 -T2 metadata.tencentyun.com/latest/meta-data/placement/zone )
+else
+PUBLICIP=$( public_ip )
+LOCALIP=$( ifconfig | grep "inet" | grep -v "127.0" | xargs | awk -F '[ :]' '{print $2}' )
+MACADDRESS=$( ifconfig | grep "ether" | awk -F" " '{print $2}' )
+fi
 
 clear
 echo -e "================================================================================="
@@ -67,5 +77,12 @@ echo -e "${RGB_INFO}Total amount of Memory ${RGB_END}: ${MEMTOTAL} (${MEMFREE} F
 echo -e "${RGB_INFO}Total amount of Swap   ${RGB_END}: ${SWAPTOTAL} (${SWAPFREE} Free)"
 echo -e "\n${RGB_WARNING}Network Overview (Contains the DNS and IP address (public and internal))${RGB_END}"
 echo -e "${RGB_INFO}Public IP              ${RGB_END}: ${PUBLICIP}"
-echo -e "${RGB_INFO}Internal IP            ${RGB_END}: ${INTERNALIP}"
+echo -e "${RGB_INFO}Local IP               ${RGB_END}: ${LOCALIP}"
+echo -e "${RGB_INFO}MAC Address            ${RGB_END}: ${MACADDRESS}"
 echo -e "${RGB_INFO}Nameserver             ${RGB_END}: ${NAMESERVER}"
+if [ ! -z "${TENCENTCLOUD}" ]; then
+echo -e "\n${RGB_WARNING}Tencent Cloud Overview (Contains the UUID, Instance ID and Zone)${RGB_END}"
+echo -e "${RGB_INFO}Instance ID            ${RGB_END}: ${INSTANCEID}"
+echo -e "${RGB_INFO}UUID                   ${RGB_END}: ${UUID}"
+echo -e "${RGB_INFO}Region & Zone          ${RGB_END}: ${REGIONZONE}"
+fi
