4cb058b6f234212f205a3dc0a06a8838b1507760
Seaton Jiang <seaton@vtrois.com>
2018-06-24 11:41:50 +0800

    Update auto_fdisk.sh
    Add the log file

--- tools/auto_fdisk.sh
+++ tools/auto_fdisk.sh
@@ -25,18 +25,25 @@ RGB_SUCCESS='✨ \033[32m'
 RGB_WARNING='💡 \033[33;1m'
 RGB_INFO='📋 \033[36;1m'
 RGB_END='\033[0m'
+LOCK=/tmp/sp_fdisk.log
 
 tool_info() {
-    echo -e "================================================================================="
-    echo -e "                         Auto fdisk tool for SpacePack                           "
-    echo -e "             For more information please visit https://spacepack.sh              "
-    echo -e "================================================================================="
+    echo -e "==========================================================================================="
+    echo -e "                              Auto fdisk tool for SpacePack                                "
+    echo -e "                  For more information please visit https://spacepack.sh                   "
+    echo -e "==========================================================================================="
 }
 
 check_root(){
     if [[ $EUID -ne 0 ]]; then
        echo -e "${RGB_DANGER}The script must be run as root!${RGB_END}"
        exit 1
+    fi
+}
+
+check_lock() {
+    if [ ! -f "$LOCK" ];then
+    touch $LOCK
     fi
 }
 
@@ -59,7 +66,7 @@ fdisk_centos() {
         RELEASE="unknown"
     fi
     if [ "${RELEASE}"=="centos" ];then
-        yum -y install e4fsprogs > /dev/null 2>&1
+        yum -y install e4fsprogs >> ${LOCK} 2>&1
     fi
 }
 
@@ -78,16 +85,16 @@ mkfs.ext4 ${1}1
 }
 
 fdisk_mounted() {
-while mount | grep "${DISK}" > /dev/null 2>&1;do
+while mount | grep "${DISK}" >> ${LOCK} 2>&1;do
     echo -e "\n${RGB_DANGER}This disk has been mounted:${RGB_END}"
     mount | grep "${DISK}"
     echo -en "\n${RGB_DANGER}Force Unloading the disk? [y/n]:${RGB_END}"
     while :; do
     read UMOUNT
-    if [[ ! "${UMOUNT}" =~ ^[y,n]$ ]]; then
+    if [[ ! "${UMOUNT}" =~ ^[y,n,Y,N]$ ]]; then
         echo -en "${RGB_DANGER}Please try again [y/n]:${RGB_END}"
     else
-        if [ "${UMOUNT}" == 'y' ]; then
+        if [ "${UMOUNT}" == 'y' ] || [ "${UMOUNT}" == 'Y' ]; then
             echo -en "${RGB_WAIT}Unloading...${RGB_END}"
             for i in `mount | grep "${DISK}" | awk '{print $3}'`;do
                 fuser -km $i >/dev/null
@@ -105,10 +112,10 @@ while mount | grep "${DISK}" > /dev/null 2>&1;do
     echo -en "\n${RGB_DANGER}Ready to format the disk? [y/n]:${RGB_END}"
     while :; do
     read CHOICE
-    if [[ ! "${CHOICE}" =~ ^[y,n]$ ]]; then
+    if [[ ! "${CHOICE}" =~ ^[y,n,Y,N]$ ]]; then
         echo -en "${RGB_DANGER}Please try again [y/n]:${RGB_END}"
     else
-        if [ "${CHOICE}" == 'y' ]; then
+        if [ "${CHOICE}" == 'y' ] ||  "${CHOICE}" == 'Y' ]; then
             echo -en "${RGB_WAIT}Formatting...${RGB_END}"
             dd if=/dev/zero of=${DISK} bs=512 count=1 &>/dev/null
             sync
@@ -122,52 +129,57 @@ while mount | grep "${DISK}" > /dev/null 2>&1;do
 done
 }
 
+fdisk_main() {
+    echo -e "\n${RGB_INFO}1/6 : Check and install the Ext4 module${RGB_END}"
+    echo -en "${RGB_WAIT}Checking...${RGB_END}"
+    fdisk_centos
+    echo -e "\r${RGB_SUCCESS}Success, the script is ready to be installed!${RGB_END}\n"
+    echo -e "${RGB_INFO}2/6 : Show all active disks${RGB_END}"
+    fdisk -l 2>/dev/null | grep -o "Disk /dev/.*vd[b-z]"
+    echo -en "\n${RGB_INFO}3/6 : Please choose the disk (e.g., /dev/vdb):${RGB_END}"
+    while :; do
+    read DISK
+    if [ -z "`echo ${DISK} | grep '^/dev/.*vd[b-z]'`" ]; then
+        echo -en "${RGB_DANGER}Please try again (e.g., /dev/vdb):${RGB_END}"
+    else
+        until fdisk -l 2>/dev/null | grep -o "Disk /dev/.*vd[b-z]" | grep "Disk ${DISK}" &>/dev/null;do
+            echo -en "${RGB_DANGER}Please try again (e.g., /dev/vdb):${RGB_END}"
+            read DISK
+        done
+        fdisk_mounted
+        break
+    fi
+    done
+    echo -e "\n${RGB_INFO}4/6 : Partitioning and formatting the disk${RGB_END}"
+    echo -en "${RGB_WAIT}Partitioning and formatting...${RGB_END}"
+    fdisk_mkfs ${DISK} >> ${LOCK} 2>&1
+    echo -e "\r${RGB_SUCCESS}Success, the disk has been partitioned and formatted!${RGB_END}\n"
+    echo -en "${RGB_INFO}5/6 : Please enter a location to mount (Default directory: /data):${RGB_END}"
+    while :; do
+    read MOUNT
+    MOUNT=${MOUNT:-"/data"}
+    if [ -z "`echo ${MOUNT} | grep '^/'`" ]; then
+        echo -en "${RGB_DANGER}The directory must begin with /, please try again (Default directory: /data):${RGB_END}"
+    else
+        echo -en "${RGB_WAIT}Mounting...${RGB_END}"
+        mkdir ${MOUNT} >> ${LOCK} 2>&1
+        mount ${DISK}1 ${MOUNT}
+        echo -e "\r${RGB_SUCCESS}Success, the mount is completed!${RGB_END}"
+        break
+    fi
+    done
+    echo -e "\n${RGB_INFO}6/6 : Write the configuration to /etc/fstab and mount the device${RGB_END}"
+    echo -en "${RGB_WAIT}Writing...${RGB_END}"
+    echo ${DISK}1 $MOUNT 'ext4 defaults 0 0' >> /etc/fstab
+    echo -e "\r${RGB_SUCCESS}Success, the /etc/fstab has been written!${RGB_END}"
+    echo -e "\n${RGB_WARNING}Show the amount of free disk space on the system${RGB_END}"
+    df -h
+    echo -e "\n${RGB_WARNING}Show the configuration file for /etc/fstab${RGB_END}"
+    cat /etc/fstab
+}
+
 clear
 tool_info
 check_root
-echo -e "\n${RGB_INFO}1/6 : Check and install the Ext4 module${RGB_END}"
-echo -en "${RGB_WAIT}Checking...${RGB_END}"
-fdisk_centos
-echo -e "\r${RGB_SUCCESS}Success, the script is ready to be installed!${RGB_END}\n"
-echo -e "${RGB_INFO}2/6 : Show all active disks${RGB_END}"
-fdisk -l 2>/dev/null | grep -o "Disk /dev/.*vd[b-z]"
-echo -en "\n${RGB_INFO}3/6 : Please choose the disk (e.g., /dev/vdb):${RGB_END}"
-while :; do
-read DISK
-if [ -z "`echo ${DISK} | grep '^/dev/.*vd[b-z]'`" ]; then
-    echo -en "${RGB_DANGER}Please try again (e.g., /dev/vdb):${RGB_END}"
-else
-    until fdisk -l 2>/dev/null | grep -o "Disk /dev/.*vd[b-z]" | grep "Disk ${DISK}" &>/dev/null;do
-        echo -en "${RGB_DANGER}Please try again (e.g., /dev/vdb):${RGB_END}"
-        read DISK
-    done
-    fdisk_mounted
-    break
-fi
-done
-echo -e "\n${RGB_INFO}4/6 : Partitioning and formatting the disk${RGB_END}"
-echo -en "${RGB_WAIT}Partitioning and formatting...${RGB_END}"
-fdisk_mkfs ${DISK} > /dev/null 2>&1
-echo -e "\r${RGB_SUCCESS}Success, the disk has been partitioned and formatted!${RGB_END}\n"
-echo -en "${RGB_INFO}5/6 : Please enter a location to mount (Default directory: /data):${RGB_END}"
-while :; do
-read MOUNT
-MOUNT=${MOUNT:-"/data"}
-if [ -z "`echo ${MOUNT} | grep '^/'`" ]; then
-    echo -en "${RGB_DANGER}The directory must begin with /, please try again (Default directory: /data):${RGB_END}"
-else
-    echo -en "${RGB_WAIT}Mounting...${RGB_END}"
-    mkdir ${MOUNT} > /dev/null 2>&1
-    mount ${DISK}1 ${MOUNT}
-    echo -e "\r${RGB_SUCCESS}Success, the mount is completed!${RGB_END}"
-    break
-fi
-done
-echo -e "\n${RGB_INFO}6/6 : Write the configuration to /etc/fstab and mount the device${RGB_END}"
-echo -en "${RGB_WAIT}Writing...${RGB_END}"
-echo ${DISK}1 $MOUNT 'ext4 defaults 0 0' >> /etc/fstab
-echo -e "\r${RGB_SUCCESS}Success, the /etc/fstab has been written!${RGB_END}"
-echo -e "\n${RGB_WARNING}Show the amount of free disk space on the system${RGB_END}"
-df -h
-echo -e "\n${RGB_WARNING}Show the configuration file for /etc/fstab${RGB_END}"
-cat /etc/fstab
+check_lock
+fdisk_main
